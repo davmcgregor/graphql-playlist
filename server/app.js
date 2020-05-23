06a3cb82210e1318534d79d647bbc3bb18c3a759
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-05-23 17:16:36 +0100

    Add mongoose

--- server/app.js
+++ server/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({
-  schema,
-  graphiql: true
-}));
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@reading-list-cluster-srbnf.mongodb.net/test?retryWrites=true&w=majority`
+);
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
   console.log("now listening for requests on port 4000");
