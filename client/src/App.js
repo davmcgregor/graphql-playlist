1f8df86bd9ee28630f889723afebcd4b2af88cfd
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-05-24 18:06:06 +0100

    Render BookList

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache()
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
-class BookList extends Component {
-  render() {
-    return (
-      <div>
-        <ul id="book-list">
-          <li>Book name</li>
-        </ul>
-      </div>
-    );
+const getBooksQuery = gql`
+  query GetBooks {
+    books {
+      name
+      id
+    }
   }
+`;
+
+function BookList() {
+  const { loading, error, data } = useQuery(getBooksQuery);
+
+  if (loading) return <p>"Loading..."</p>;
+  if (error) return <p>`Error! ${error.message}`</p>;
+
+  return (
+    <div>
+      <ul id="book-list">
+        {data.books.map(book => (
+          <li key={book.id}>{book.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default BookList;
